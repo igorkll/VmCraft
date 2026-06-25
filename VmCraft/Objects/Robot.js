dd6a7aa6ac735ed1500fcb4d2c0a3323d26e9386
igorkll <frerther@gmail.com>
2026-06-26 00:51:53 +0300

    Update Robot.js

--- VmCraft/Objects/Robot.js
+++ VmCraft/Objects/Robot.js
@@ -2,18 +2,23 @@ import * as Three from "three";
 import * as Utils from "../Utils.js";
 
 const geometry = new Three.BoxGeometry(0.8, 0.8, 0.8)
-const material = new Three.MeshLambertMaterial({ color: 0x444444 })
+const material = new Three.MeshLambertMaterial({ color: 0x666666 })
+
+const geometry2 = new Three.BoxGeometry(0.4, 0.4, 0.4)
+const material2 = new Three.MeshLambertMaterial({ color: 0xff0000 })
 
 export class Robot {
     constructor(scene, x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        this.scene = scene
+        this.x = x
+        this.y = y
+        this.z = z
 
         this.object = new Three.Mesh(geometry, material)
         scene.add(this.object)
 
-        const cube2 = new Three.Mesh(geometry, material)
+        const cube2 = new Three.Mesh(geometry2, material2)
+        cube2.position.set(0.3, 0.1, 0);
         this.object.add(cube2)
     }
 
@@ -22,6 +27,6 @@ export class Robot {
     }
     
     destroy() {
-        scene.remove(this.object)
+        this.scene.remove(this.object)
     }
 }
